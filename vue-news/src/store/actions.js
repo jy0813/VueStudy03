12a5ad07ba620838bdfe494be96daec2db912a01
jy0813 <busbug3@gmail.com>
2022-09-19 13:09:36 +0900

    feat: store 모듈화

---
+++ vue-news/src/store/actions.js
@@ -0,0 +1,19 @@
+import { fetchNewsList, fetchAskList, fetchJobsList } from "../api/index.js";
+
+export default {
+  FETCH_NEWS(context) {
+    fetchNewsList()
+      .then((res) => context.commit("SET_NEWS", res.data))
+      .catch((err) => console.log(err));
+  },
+  FETCH_ASK({ commit }) {
+    fetchAskList()
+      .then(({ data }) => commit("SET_ASK", data))
+      .catch((err) => console.log(err));
+  },
+  FETCH_JOBS({ commit }) {
+    fetchJobsList()
+      .then(({ data }) => commit("SET_JOBS", data))
+      .catch((err) => console.log(err));
+  },
+};
--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { fetchNewsList, fetchAskList, fetchJobsList } from "../api/index.js";
+import mutations from "./mutations";
+import actions from "./actions";
 Vue.use(Vuex);
 
 //상태관리 도구
@@ -15,32 +16,6 @@ export const store = new Vuex.Store({
       return state.ask;
     },
   },
-  mutations: {
-    SET_NEWS(state, news) {
-      state.news = news;
-    },
-    SET_ASK(state, ask) {
-      state.ask = ask;
-    },
-    SET_JOBS(state, jobs) {
-      state.jobs = jobs;
-    },
-  },
-  actions: {
-    FETCH_NEWS(context) {
-      fetchNewsList()
-        .then((res) => context.commit("SET_NEWS", res.data))
-        .catch((err) => console.log(err));
-    },
-    FETCH_ASK({ commit }) {
-      fetchAskList()
-        .then(({ data }) => commit("SET_ASK", data))
-        .catch((err) => console.log(err));
-    },
-    FETCH_JOBS({ commit }) {
-      fetchJobsList()
-        .then(({ data }) => commit("SET_JOBS", data))
-        .catch((err) => console.log(err));
-    },
-  },
+  mutations: { mutations },
+  actions: { actions },
 });
